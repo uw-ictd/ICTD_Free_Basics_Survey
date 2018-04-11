520b38abd9b3e48bed3cb124e3063f7b7cb7f923
Sarah Oslund <sfoslund@gmail.com>
2018-04-11 09:46:28 -0700

    Added templates (see SampleSurvey/templates)

--- SampleSurvey/SampleSurvey/settings.py
+++ SampleSurvey/SampleSurvey/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+SETTINGS_PATH = os.path.normpath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'SampleSurvey.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(SETTINGS_PATH, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- SampleSurvey/Survey/views.py
+++ SampleSurvey/Survey/views.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.template.loader import get_template
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Survey Home")
+    # We'll prob have to put some database initialization stuff here
+    template = get_template("home.html")
+    #context = Context({});
+    #return HttpResponse(template.render(context));
+    return HttpResponse(template.render());
